36faf62886b5a111b6e88fc8bbbfd75ccd272121
Josh Harry <josh.c.harry@outlook.com>
2025-04-02 12:14:58 +0800

    create token WiP

--- JWDIACONTRACTS/Services/Auth/AuthService.cs
+++ JWDIACONTRACTS/Services/Auth/AuthService.cs
@@ -1,26 +1,45 @@
+using System.Runtime.CompilerServices;
+using System.Reflection;
+using System.Text;
+using System.IO.Enumeration;
+using System.IO;
 using System;
-using JWDIACONTRACTS.Interfaces.Auth;
-using JWDIACONTRACTS.DTO.Auth;
+using System.Security.AccessControl;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Configuration;
+
+
+// using System.IdentityModel.Tokens.JWT;
 using JWDIADATA.Data.Entities;
 using JWDIADATA.Data;
-using Microsoft.EntityFrameworkCore;
-using System.Linq;
-// using System.IdentityModel.Tokens.JWT;
 using JWDIACONTRACTS.Mappings.Auth;
+using JWDIACONTRACTS.Interfaces.Auth;
+using JWDIACONTRACTS.DTO.Auth;
+
 
 
 namespace JWDIACONTRACTS.Services.Auth;
 public class AuthService : IAuthService
 {
     private readonly ApplicationDbContext _context;
+    private readonly IConfiguration _configuration;
 
-    public AuthService(ApplicationDbContext context)
+    public AuthService(ApplicationDbContext context, IConfiguration configuration)
     {
         _context = context;
+        _configuration = configuration;
     }
 
 
     public async Task<UserDTO> CreateUserAsync (NewUser userIn) {
+
+        
 
         UserDataModel user = new UserDataModel(){
             Username = userIn.Username,
@@ -28,8 +47,10 @@ public class AuthService : IAuthService
             RequireUniqueEmail = userIn.RequireUniqueEmail,
             Role = "User"
         };
- 
-        // hash the password
+
+        var hashedPassword = new PasswordHasher<UserDataModel>().HashPassword(user, user.Password);
+        user.Password = hashedPassword;
+
 
         UserDataModel checkUser = await _context.Users.SingleOrDefaultAsync(u => u.Username == userIn.Username);
         
@@ -53,20 +74,52 @@ public class AuthService : IAuthService
 
     public async Task<string> LoginAsync (Login userLogin) {
 
-        // hash password
-        // check exists
+        UserDataModel checkUser = await _context.Users.SingleOrDefaultAsync(u => u.Username == userLogin.UserName);
+        if (checkUser == null){
+
+            return null;
+        }
+        
+        if (new PasswordHasher<UserDataModel>().VerifyHashedPassword(checkUser, checkUser.Password, userLogin.Password)
+           == PasswordVerificationResult.Failed){
+            return null;
+        }
+
+        string token = CreateToken(checkUser);
         // create a token
         // return the token
 
-        return null ;
+        return token;
     }
 
 
 
-    private async Task<string> CreateToken (UserDTO user) {
 
+    private string CreateToken (UserDataModel user) {
 
-        return "I am a token";
+        var claims = new List<Claim>{
+            new Claim(ClaimTypes.Name, user.Username)
+        };
+
+        // // var key = configuration.GetValue<string>("AppSettings:Token");
+        // // var issuer = configuration.GetValue<string>("AppSettings:Issuer");
+        // // var audience = configuration.GetValue<string>("AppSettings:Audience");
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("tokentoketokentoketokentoketokentoketokentoketokentoketokentoketokentoke"));
+        
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+        
+        var tokenDescriptor = new JwtSecurityToken(
+            issuer: "Me",
+            audience: "ThePeople",
+            claims: claims,
+            expires: DateTime.UtcNow.AddDays(1),
+            signingCredentials: creds
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+
+        
     }
     
 }
